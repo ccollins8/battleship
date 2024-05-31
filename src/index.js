426c876447930ab342577e62845963216ea3c6d8
ccollins8 <48657302+ccollins8@users.noreply.github.com>
2024-05-30 18:58:33 -0700

    Add switch turns

--- src/index.js
+++ src/index.js
@@ -2,28 +2,59 @@ import "./style.css";
 import Dom from './modules/Dom'
 import Ship from "./modules/Ship";
 import Gameboard from "./modules/Gameboard";
+import Player from "./modules/Player";
 
-const gb = new Gameboard
-const ship1 = new Ship(4)
-const ship2 = new Ship(3)
-const ship3 = new Ship(2)
-const ship4 = new Ship(1)
+const p1 = new Player('corey',true)
+const p2 = new Player('bot', false)
 
-gb.placeShip(ship1, 3, 2, true)
-gb.placeShip(ship2, 6, 4, false)
-gb.placeShip(ship3, 4, 7, false)
-gb.placeShip(ship4, 7, 7, true)
+let CurrentPlayer = p1
+
+// p1 ships
+const p1ship1 = new Ship(4)
+const p1ship2 = new Ship(3)
+const p1ship3 = new Ship(2)
+const p1ship4 = new Ship(1)
+
+p1.gameboard.placeShip(p1ship1, 5, 2, true)
+p1.gameboard.placeShip(p1ship2, 8, 6, false)
+p1.gameboard.placeShip(p1ship3, 4, 7, false)
+p1.gameboard.placeShip(p1ship4, 7, 8, true)
+
+// p2 ships
+const p2ship1 = new Ship(4)
+const p2ship2 = new Ship(3)
+const p2ship3 = new Ship(2)
+const p2ship4 = new Ship(1)
+
+p2.gameboard.placeShip(p2ship1, 3, 2, true)
+p2.gameboard.placeShip(p2ship2, 6, 4, false)
+p2.gameboard.placeShip(p2ship3, 4, 7, false)
+p2.gameboard.placeShip(p2ship4, 7, 7, true)
 
 const left = document.querySelector('.left')
+const right = document.querySelector('.right')
 
-Dom.renderBoard(gb)
+Dom.renderBoard(p1.gameboard, left)
+Dom.renderBoard(p2.gameboard, right)
 
 left.addEventListener("click", (e) => {
-    if (e.target.classList.contains("cell")) {
+    if (e.target.classList.contains("cell") && CurrentPlayer == p1) {
         const split_coords = e.target.id.split(',')
         const x = parseInt(split_coords[0])
         const y = parseInt(split_coords[1])
-        gb.receiveAttack(x,y)
-        Dom.renderBoard(gb)
+        p1.gameboard.receiveAttack(x,y)
+        CurrentPlayer = p2
+        Dom.renderBoard(p1.gameboard, left)
+    }
+})
+
+right.addEventListener("click", (e) => {
+    if (e.target.classList.contains("cell") && CurrentPlayer == p2) {
+        const split_coords = e.target.id.split(',')
+        const x = parseInt(split_coords[0])
+        const y = parseInt(split_coords[1])
+        p2.gameboard.receiveAttack(x,y)
+        CurrentPlayer = p1
+        Dom.renderBoard(p2.gameboard, right)
     }
 })
